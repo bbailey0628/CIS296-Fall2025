006aeeb354e4433878f40b875e402d752557ba59
Eric Charnesky <EricCharnesky@gmail.com>
2025-08-28 20:07:22 -0400

    week 1

---
+++ Week1/src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Eric Charnesky ( he/him )");
+        System.out.println("Doctoral dropout");
+
+        System.out.println("I'm training for my 11th marathon in October");
+        System.out.println("medical imaging technology and screening");
+
+        String name = "Eric";
+        int favoriteNumber = 42;
+
+        System.out.println(name + "'s favorite number is " + favoriteNumber);
+
+        // integer divided by integer will give an integer result - NO DECIMALS
+        System.out.println(favoriteNumber / 5 );
+
+        double numberWithDecimal = 7.7;
+
+        System.out.println(numberWithDecimal / 2);
+
+        float numberWithLessDecimalPrecision= 7.7F;
+
+        char letter = 'E';
+
+        System.out.println(letter + favoriteNumber);
+    }
+}
