40d7af54dc773085b1ce7f822d25b66ef56b55bc
bbailey0628 <bbailey0628@hotmail.com>
2025-09-02 17:51:38 -0400

    Project0 Final

--- Week1/src/Main.java
+++ Week1/src/Main.java
@@ -1,27 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Eric Charnesky ( he/him )");
-        System.out.println("Doctoral dropout");
-
-        System.out.println("I'm training for my 11th marathon in October");
-        System.out.println("medical imaging technology and screening");
-
-        String name = "Eric";
-        int favoriteNumber = 42;
-
-        System.out.println(name + "'s favorite number is " + favoriteNumber);
-
-        // integer divided by integer will give an integer result - NO DECIMALS
-        System.out.println(favoriteNumber / 5 );
-
-        double numberWithDecimal = 7.7;
-
-        System.out.println(numberWithDecimal / 2);
-
-        float numberWithLessDecimalPrecision= 7.7F;
-
-        char letter = 'E';
-
-        System.out.println(letter + favoriteNumber);
+        System.out.println("Moussa Kabalan ( he/him )");
+        System.out.println("Computer Engineering");
+        System.out.println("Junior in degree path + personal projects");
+        System.out.println("Loves to tinker with electronics to learn how they work");
+        System.out.println("Use of AI in hospitals");
     }
 }
